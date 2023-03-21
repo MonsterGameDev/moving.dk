78d3efdcb7e1ba8d8336c3949b0400059cfcb4ac
MonsterGameDev <per@brf.dk>
2023-03-21 21:36:24 +0100

    corrections

--- src/img/index.js
+++ src/img/index.js
@@ -5,6 +5,7 @@ import FlytteKasser from './webp/gratis-flyttekasser.webp'
 import Inspection from './webp/inspection.webp'
 import Montage from './webp/moebel-montage.webp'
 import Opbevaring from './webp/2flyttemaend.webp'
+import Logo from './moving-dk-logo.png';
 
 export default {
     FlytteMaendPicRow,
@@ -13,40 +14,8 @@ export default {
     FlytteKasser,
     Inspection,
     Montage,
-    Opbevaring
+    Opbevaring,
+    Logo
 }
 
 
-// import UngtPar from './2ppsAndBoxes.webp';
-// import Logo from './moving-dk-logo.png';
-// import FlytteKasser_1 from './flyttekasser i bil.jpg'
-// import FlytteKasser_2 from './gratis-flyttekasser.jpg'
-// import FlytteKasser_3 from './gratis-flyttekasser-2.jpg'
-// import FlytteKasser_4 from './gratis-flyttekasser-3.jpg'
-// import Inspection from './inspection.jpg';
-// import Koersel_1 from './koersel-af-flyttelaes.jpg';
-// import Koersel_2 from './koersel-af-flyttelaes-2.jpg';
-// import Koersel_3 from './koersel-af-flyttelaes-3.jpg';
-// import Montage_1 from './moebel-montage.jpg';
-// import Montage_2 from './montage.jpg';
-// import Montage_3 from './moebel-montage-2.webp';
-// import Opbevaring from './opbevaring.jpg';
-
-
-// export default {
-//     FlytteMaend,
-//     UngtPar,
-//     Logo,
-//     FlytteKasser_1,
-//     FlytteKasser_2,
-//     FlytteKasser_3,
-//     FlytteKasser_4,
-//     Inspection,
-//     Koersel_1,
-//     Koersel_2,
-//     Koersel_3,
-//     Montage_1,
-//     Montage_2,
-//     Montage_3,
-//     Opbevaring
-// }

5be0ce2549afefbddff54577069c94b683ba02e8
MonsterGameDev <per@brf.dk>
2023-09-06 17:26:50 +0200

    changed fontweight

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import ShowCaseSection from './components/showcase/showcase.js'
 import ContactForm from './components/contact/contact.js';
 import './web-components/testimonial-image-circle.js'
 import Testimonials from './components/testimonials/testimonials.js';
+import FixedCtaBox from './components/fixed-cta-box/fixed-cta-box';
+
+const ctabox = new FixedCtaBox();
+ctabox.render();
 
 const homeSection = new HomeSection();
 homeSection.render();

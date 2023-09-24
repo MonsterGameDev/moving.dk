fc2d5f881ab49c0d0ca605ef282cb70fe695bc41
MonsterGameDev <per@brf.dk>
2023-09-24 16:34:03 +0200

    galleri image barrel

---
+++ src/img/galleri/index.js
@@ -0,0 +1,23 @@
+import GALLERI_1 from './1.webp';
+import GALLERI_2 from './2.webp';
+import GALLERI_3 from './3.webp';
+import GALLERI_4 from './4.webp';
+import GALLERI_5 from './5.webp';
+import GALLERI_6 from './6.webp';
+import GALLERI_7 from './7.webp';
+import GALLERI_8 from './8.webp';
+import GALLERI_9 from './9.webp';
+import GALLERI_10 from './10.webp';
+
+export default {
+    GALLERI_1,
+    GALLERI_2,
+    GALLERI_3,
+    GALLERI_4,
+    GALLERI_5,
+    GALLERI_6,
+    GALLERI_7,
+    GALLERI_8,
+    GALLERI_9,
+    GALLERI_10
+}
